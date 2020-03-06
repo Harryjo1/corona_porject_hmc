4c43963a5b63a5bd2b0a51bfef0f38f3bfed78a2
Harryjo1 <60224669+Harryjo1@users.noreply.github.com>
2020-03-06 17:03:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -155,7 +155,7 @@ def graphique_Nb_enfant(annee):
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-#server =app.server
+server =app.server
 
 
 app.layout = html.Div([
